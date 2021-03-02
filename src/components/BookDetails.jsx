62631cb80b8e6b65789eda531d9e0059d4e7e0ff
Gerardo Tordoya <gtordoya@TRIBJUY.local>
2021-03-02 19:18:28 -0300

    Done!

---
+++ src/components/BookDetails.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import { getBookQuery } from "../queries/queries";
+
+class BookDetails extends Component {
+  displayBookDetails() {
+    const { book } = this.props.data;
+    if (book) {
+      return (
+        <div>
+
+          <h2>{book.name}</h2>
+          <p>{book.genre}</p>
+          <p>{book.author.name}</p>
+          <p>Otros libros de este autor:</p>
+          <ul className="other-books">
+            {book.author.books.map((item) => {
+              return <li key={item.id}>{item.name}</li>;
+            })}
+          </ul>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+
+          <code>Ningún libro seleccionado.</code>
+        </div>
+      );
+    }
+  }
+  render() {
+    return <div id="book-details">{this.displayBookDetails()}</div>;
+  }
+}
+
+export default graphql(getBookQuery, {
+  options: (props) => {
+    return {
+      variables: {
+        id: props.bookId,
+      },
+    };
+  },
+})(BookDetails);
--- src/queries/queries.js
+++ src/queries/queries.js
@@ -27,8 +27,28 @@ const addBookMutation = gql`
     }
 `;
 
+const getBookQuery = gql`
+  query($id: ID){
+    book(id: $id){
+      id
+      name
+      genre
+      author{
+        id
+        name
+        age
+        books{
+          id
+          name
+        }
+      }
+    }
+  }
+`
+
 export {
     getAuthorsQuery,
     getBooksQuery,
-    addBookMutation
+    addBookMutation,
+    getBookQuery
 }
